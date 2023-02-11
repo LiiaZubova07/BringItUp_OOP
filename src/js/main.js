832a02130c4d5c96f67081db98723b641912efb8
Liia Zubova <vardlitmo@gmail.com>
2023-02-11 18:20:24 +0300

    add files

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+import Slider from "./modules/slider";
+
+window.addEventListener("DOMContentLoaded", () => {
+  const slider = new Slider(".page", ".next");
+
+  console.log(123)
+  slider.render();
+});
