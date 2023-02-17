d500267b9d630e457b461af37830030933463c93
Liia Zubova <vardlitmo@gmail.com>
2023-02-17 19:09:50 +0300

    add: open/blocked video

--- src/js/modules/playVideo.js
+++ src/js/modules/playVideo.js
@@ -3,17 +3,33 @@ export default class VideoPlayer {
     this.btns = document.querySelectorAll(triggers);
     this.overlay = document.querySelector(overlay);
     this.close = document.querySelector(".close");
+    this.onPlayerStateChange = this.onPlayerStateChange.bind(this);
   }
 
   bindTriggers() {
-    this.btns.forEach((btn) => {
-      btn.addEventListener("click", () => {
+    this.btns.forEach((btn, i) => {
+      const blockedElem = btn.closest(".module__video-item").nextElementSibling;
 
-        if (document.querySelector("iframe#frame")) {
-          this.overlay.style.display = "flex";
-        } else {
-          const path = btn.getAttribute("data-url");
-          this.createPlayer(path);
+      if (i % 2 == 0) {
+        blockedElem.setAttribute("data-disabled", "true");
+      }
+
+      btn.addEventListener("click", () => {
+        //блокировка элемента
+        if (btn.closest(".module__video-item").getAttribute("data-disabled") !== "true") {
+          this.activeBtn = btn;
+
+          if (document.querySelector("iframe#frame")) {
+            this.overlay.style.display = "flex";
+            if (this.path !== btn.getAttribute("data-url")) {
+              this.path = btn.getAttribute("data-url");
+              this.player.loadVideoById({ videoId: this.path });
+            }
+          } else {
+            this.path = btn.getAttribute("data-url");
+
+            this.createPlayer(this.path);
+          }
         }
       });
     });
@@ -31,13 +47,38 @@ export default class VideoPlayer {
       height: "100%",
       width: "100%",
       videoId: `${url}`,
+      events: {
+        onStateChange: this.onPlayerStateChange,
+      },
     });
 
-    console.log(this.player);
     this.overlay.style.display = "flex";
   }
 
+  onPlayerStateChange(state) {
+    const blockedElem = this.activeBtn.closest(".module__video-item").nextElementSibling;
+    const playBtn = this.activeBtn.querySelector("svg").cloneNode(true);
+
+    if (state.data === 0) {
+      if (blockedElem.querySelector(".play__circle").classList.contains("closed")) {
+        blockedElem.querySelector(".play__circle").classList.remove("closed");
+        blockedElem.querySelector("svg").remove();
+        blockedElem.querySelector(".play__circle").appendChild(playBtn);
+        blockedElem.querySelector(".play__text").textContent = "play video";
+        blockedElem.querySelector(".play__text").classList.remove(".attention");
+        blockedElem.style.opacity = 1;
+        blockedElem.style.filter = "none";
+
+        //разблокировка элемента
+        blockedElem.setAttribute("data-disabled", "false");
+      }
+    }
+  }
+
   init() {
+    if (!this.btns.length) {
+      return;
+    }
     const tag = document.createElement("script");
 
     tag.src = "https://www.youtube.com/iframe_api";
